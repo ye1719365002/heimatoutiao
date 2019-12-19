93875b71f77a53950cbb6e38a9cf20ce1a5cd441
ye1719365002 <1719365002@qq.com>
2019-12-19 21:22:58 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,23 +6,23 @@
   <div class="title">
     <img src="../../assets/img/logo_index.png" alt="">
   </div>
-  <!-- 放置表单 -->
-    <el-form>
-      <!-- 表单域 -->
-      <el-form-item>
-        <el-input placeholder="请输入手机号"></el-input>
+  <!-- 放置表单 绑定数据对象 el-form-model-->
+    <el-form ref="myForm" :model="loginForm" :rules="loginRules" >
+      <!-- 表单域 里面 porp要写要检验的字段名 放置 input/select/checkbox 相当于一行 -->
+      <el-form-item prop="mobile">
+        <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
       <!-- 表单域 -->
-      <el-form-item>
-        <el-input style="width:65%" placeholder="请输入验证码"></el-input>
-        <el-button style="float:right " plain>发送验证码</el-button>
+      <el-form-item prop="code">
+        <el-input v-model="loginForm.code" style="width:65%" placeholder="请输入验证码"></el-input>
+        <el-button style="float:right " >发送验证码</el-button>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="check">
         <!-- 复选框 -->
-        <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
       </el-form-item>
       <el-form-item>
-           <el-button type="primary" style="width:100% " >登录</el-button>
+           <el-button @click="submitLogin" type="primary" style="width:100% " >登录</el-button>
       </el-form-item>
     </el-form>
     </el-card>
@@ -31,7 +31,50 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false//  是否勾选 同意被坑
+      },
+      loginRules: {
+        // 验证规则对象 key(字段名)：value(规则 =>[])
+        mobile: [{ required: true, message: '请输入你的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号码格式不对' }
+        ],
+        code: [{ required: true, message: '请输入你的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式不正确' }
+        ],
+        check: [{ validator: function (rule, value, callback) {
+        // 自定义校验函数
+        // rule 规则 没什么用
+        // value 要校验的字段的值
+        // callback 是一个回调函数
+          if (value) {
+          // 认为已经勾选
+            callback()// 认为当前的规则校验通过了
+          } else {
+          // 认为没有勾选
+            callback(new Error('您应该同意我们的霸王条款'))// 如果没有勾选认为当前校验失败 应该停止
+          }
+        } }]
+        // agree: [{ validator }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      // 手动校验
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          // 说明校验通过 应该调用登录接口
+          console.log('校验通过，开始调用登录接口')
+        }
+      })
+    }
 
+  }
 }
 </script>
 
