6b3bd18448f4fd41f31fdc5d261018ab0331e45f
ye1719365002 <1719365002@qq.com>
2019-12-20 16:10:16 +0800

    主页模块-头部结构及下拉菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,16 @@
 <!-- 放置一个容器 -->
     <el-container>
       <!-- 左右布局 -->
-        <el-aside>
-          左侧内容
+        <el-aside style="background-color:#323745; height:100vh;width:230px">
+          <layout-aside></layout-aside>
         </el-aside>
         <!-- 右侧容器 -->
     <el-container>
       <!-- 上下布局 -->
-      <el-header>头部</el-header>
+      <el-header>
+        <!-- 头部组件 -->
+        <layout-header></layout-header>
+      </el-header>
       <el-main>
        主要区域
       </el-main>
@@ -18,8 +21,14 @@
 </template>
 
 <script>
-export default {
+import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 
+export default {
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
 }
 </script>
 
