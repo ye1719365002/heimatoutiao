5e0680691512165081a234dd4cc671c1b7751441
ye1719365002 <1719365002@qq.com>
2019-12-26 16:39:44 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-card>
+    <el-card class="articles">
         <bread-crumb slot="header">
         <template slot="title">文章列表</template>
         </bread-crumb>
@@ -27,6 +27,27 @@
                  <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
             </el-form-item>
         </el-form>
+        <el-row class="total" type='flex' align='middle' >
+            <span>
+                共找到10000条符合条件的内容
+            </span>
+        </el-row>
+        <div class='article-item' v-for="item in 100" :key="item">
+            <!-- 左侧 -->
+            <div class="left">
+                <img src="../../assets/img/default.gif" alt="">
+                <div class="info">
+                    <span>89期的臭妹妹</span>
+                    <el-tag class="tag">标签一</el-tag>
+                    <span class="data">2019-12-25 16:26:01</span>
+                </div>
+            </div>
+            <!-- 右侧 -->
+            <div class="right">
+                <span><i class="el-icon-edit"></i>修改</span>
+                <span><i class="el-icon-delete"></i>删除</span>
+            </div>
+        </div>
     </el-card>
 </template>
 
@@ -59,6 +80,47 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+    .articles{
+        .total{
+            height: 60px;
+            border-bottom:1px dashed #ccc;
+        }
+        .article-item{
+            display: flex;
+            justify-content: space-between;
+            padding: 20px 0 ;
+            border-bottom: 1px solid #f2f3f5;
+            .left{
+                display: flex;
+                img{
+                    width: 180px;
+                    height: 100px;
+                    border-radius: 4px;
+                }
+                .info{
+                    height: 100px;
+                    margin-left: 10px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-around;
+                    .data{
+                        color: #999;
+                        font-size: 12px;
+                    }
+                    .tag{
+                        text-align: center;
+                        width:60px;
+                    }
+                }
+            }
+            .right{
+                span{
+                    font-size: 14px;
+                    margin-right: 8px;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
 </style>
